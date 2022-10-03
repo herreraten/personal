4133678e59f58509e54d9ddb2c4bcbfd2df121da
herreraten <112837135+herreraten@users.noreply.github.com>
2022-10-03 10:08:28 +0800

    Create App.java

---
+++ week 8/do while/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        int rideCount = 5;
+
+        do {
+            System.out.println("Tenten Herrera");
+            rideCount--;
+        } while (rideCount != 0);
+    }
+}
