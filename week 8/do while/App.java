b5aab18198404d3fa16c5362227ff53d1d285955
herreraten <112837135+herreraten@users.noreply.github.com>
2022-10-03 10:06:19 +0800

    Create App.java

---
+++ week 8/do while/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        int rideCount = 5;
+
+        do {
+            System.out.println("Tenten pogi");
+            rideCount--;
+        } while (rideCount != 0);
+    }
+}
