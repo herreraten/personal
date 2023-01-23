401fbf4d2d60693d84876845e6ce7906290aa347
herreraten <112837135+herreraten@users.noreply.github.com>
2023-01-23 13:04:44 +0800

    Create App.java

---
+++ 2nd sem/loop/App.java
@@ -0,0 +1,24 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        for (int i = 5; i != 0; i--) {
+            System.out.println("CHRISTENNE");
+            
+        }
+        
+        int nicknameCount = 5;
+
+        while (nicknameCount != 0) {
+            System.out.println("TENTEN");
+            nicknameCount--;
+        }
+        
+         int nameCount = 5;
+
+        do {
+            System.out.println("HERRERA");
+            nameCount--;
+        } while (nameCount != 0);
+        
+    }
+}
