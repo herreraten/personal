c3cc668d4ae726e5a85ffa2e3612ee1bf42fdb15
herreraten <112837135+herreraten@users.noreply.github.com>
2023-01-23 15:44:55 +0800

    Create App.java

---
+++ 2nd sem/basic enter element array/App.java
@@ -0,0 +1,34 @@
+package machineproblem2_herrera;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author herreracc
+ */
+public class MachineProblem2_Herrera {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+         
+         System.out.print("Enter the size of the storage: ");
+        int n = sc.nextInt();
+        System.out.print("\n");
+        //size
+        int[] num = new int[n];
+        //input
+        for(int i = 0; i < n; i++){
+            System.out.print("Enter an element for index[" + (i+1) + "]: ");
+            num[i] = sc.nextInt();
+        }
+        //output
+        System.out.println("\nIndex\t==>\tElement");
+        for(int i = 0; i < n; i++){
+            System.out.println(i + "\t==>\t" + num[i]);
+        }
+    }
+    
+}
